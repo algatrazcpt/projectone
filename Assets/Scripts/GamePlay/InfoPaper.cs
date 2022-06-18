4bdd8a4ed779d509033b785e18119964891e0986
algatrazcpt <53114980+algatrazcpt@users.noreply.github.com>
2022-06-18 14:14:46 +0300

    dialog fix1

--- Assets/Scripts/GamePlay/InfoPaper.cs
+++ Assets/Scripts/GamePlay/InfoPaper.cs
@@ -4,11 +4,26 @@ using UnityEngine;
 
 public class InfoPaper : Interactable
 {
-
+    int index = 0;
+    public void Start()
+    {
+        index = CharacterInfoControl.Instance.currentDialog;
+        CharacterInfoControl.Instance.NextDialog();
+        
+    }
     public override void Interact()
     {
-        CharacterInfoControl.Instance.NextDialog();
-        Debug.Log("InfoPaper.Interact()");
+        if (CharacterInfoControl.Instance.allCharacters[index].PatientFinish)
+        {
+            CharacterInfoControl.Instance.PatientGet(index);
+            CharacterInfoControl.Instance.NextDialog();
+            Debug.Log("InfoPaper.Interact()");
+        }
+        else
+        {
+            CharacterInfoControl.Instance.PatientGet(index);
+            Debug.Log("InfoPaper.Interact()");
+        }
     }
 
 }
--- Assets/Scripts/PauseControl.cs
+++ Assets/Scripts/PauseControl.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class PauseControl : MonoBehaviour
 {
-
+    public static bool pauseMenuShow = true;
     public void GamePause()
     {
-        Time.timeScale = 0;
-        SceneManager.LoadSceneAsync("PauseScene",LoadSceneMode.Additive);
+        if (pauseMenuShow)
+        {
+            Time.timeScale = 0;
+            SceneManager.LoadSceneAsync("PauseScene", LoadSceneMode.Additive);
+            pauseMenuShow=false;
+        }
+        
     }
 }
